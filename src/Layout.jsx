80580622db514b98125517d40ea3024dae5cbcea
Your Name <85749511+DEVOtavioSantos@users.noreply.github.com>
2026-03-12 16:17:02 -0300

    Responsivo 3

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom"
 import NavBar from "./components/NavBar/NavBar.jsx"
-
+import Footer from "./components/footer/footer.jsx"
 function Layout() {
   return (
     <>
       <NavBar />
       <Outlet />
+      <Footer/>
     </>
   )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,11 +7,14 @@ function NavBar() {
    
 
     return (
-        <nav className="flex-row bg-white dark:bg-gray-800 flex  justify-around items-center p-10 border-solid border-b-2 border-black shadow-x1 flex-wrap">
-            <>
-                <Link to="/" className="text-8x1 text-gray-800 dark:text-white text-8x1" >Logo</Link>
+        <nav className="flex-row bg-white dark:bg-gray-800 flex  justify-around items-center p-10 border-solid border-b-2 border-black shadow-x1  flex-wrap">
+            <>  
+                <Link to="/" className="text-8x1 text-gray-800 dark:text-white text-8x1 m-5" >Trabalho de Semínarios 3</Link>
                 <div className="flex items-center relative inline-blcok gap-4 ">
                     <DropDown text="Objetivo" items={Objetivo}/>
+                    <DropDown text="Objetivo" items={Objetivo}/>
+                    <DropDown text="Objetivo" items={Objetivo}/>
+                    
                     
                 </div>
             </>
